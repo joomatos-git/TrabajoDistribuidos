6ee3fc62645dfb22ceeffd3f30988cff079a738e
ChonChoner <117522514+ChonChoner@users.noreply.github.com>
2025-12-05 19:31:58 +0100

    Action a Tile en vez de coordenadas. Tile hecha en teoria

--- src/Acciones/Action.java
+++ src/Acciones/Action.java
@@ -1,29 +1,27 @@
 package Acciones;
 
+import GameObjects.Tile;
+
 public class Action {
     private ActionType type;
-    private int fromRow;
-    private int fromCol;
-    private int toRow;
-    private int toCol;
+    private Tile origin;
+    private Tile destination;
     
-    public Action(ActionType type, int fromRow, int fromCol, int toRow, int toCol) {
-        this.type = type;
-        this.fromRow = fromRow;
-        this.fromCol = fromCol;
-        this.toRow = toRow;
-        this.toCol = toCol;
+    
+    public Action(ActionType t, Tile o, Tile d) {
+    	this.type=t;
+    	this.origin=o;
+    	this.destination=d;
     }
     
     // Getters
     public ActionType getType() { return type; }
-    public int getFromRow() { return fromRow; }
-    public int getFromCol() { return fromCol; }
-    public int getToRow() { return toRow; }
-    public int getToCol() { return toCol; }
+    public Tile getOrigin() { return origin; }
+    public Tile getDestination() { return destination; }
+    
     
     @Override
     public String toString() {
-        return type + ": (" + fromRow + "," + fromCol + ") -> (" + toRow + "," + toCol + ")";
+        return type + ": " + origin.toString() + "->" + destination.toString();
     }
 }
--- src/GameObjects/Tile.java
+++ src/GameObjects/Tile.java
@@ -1,5 +1,40 @@
 package GameObjects;
 
 public class Tile {
+    private int row;
+    private int col;
+    private Piece piece;  // null si está vacía
+    
+    public Tile(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.piece = null;
+    }
+    
+    public boolean isEmpty() {
+        return piece == null;
+    }
+    
+    // Getters y setters
+    public Piece getPiece() { return piece; }
+    public void setPiece(Piece piece) { this.piece = piece; }
+    public int getRow() { return row; }
+    public int getCol() { return col; }
+    
+    
+    @Override
+    public String toString() {
+    	return getCharForNumber(col)+row;
+    }
+    
+    
+    
+    private String getCharForNumber(int i) {
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        if (i > 25) {
+            return null;
+        }
+        return Character.toString(alphabet[i]);
+    }
 
 }
